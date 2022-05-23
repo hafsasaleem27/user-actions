c2addf3e408d2ed8e5b8c0ec8736fd3bfb22fd7d
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-05-24 00:36:56 +0500

    added AddUser CSS module

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from "react";
+import Card from "../UI/Card/Card";
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
-      event.preventDefault();
+    event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Your name</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Your age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Your name</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Your age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
