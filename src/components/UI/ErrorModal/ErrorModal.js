41589746f02cf3c7a88641001bd4e58eadd4d675
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-05-25 00:04:27 +0500

    Added Error Modal Component and its .module.css file and linked both

---
+++ src/components/UI/ErrorModal/ErrorModal.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import styles from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+export default ErrorModal;
